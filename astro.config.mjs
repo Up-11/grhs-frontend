fd2d8363a900c2bf57e820a233f36ce0f439836e
up-11 <iurykolgin@gmail.com>
2025-04-05 21:38:17 +0700

    add host

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,37 +4,41 @@ import tailwindcss from '@tailwindcss/vite'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
 export default defineConfig({
-    viewTransitions: true,
-    vite: {
-        plugins: [tailwindcss()],
-        resolve: {
-            alias: {
-                '@components': '/src/components',
-                '@layouts': '/src/layouts',
-                '@pages': '/src/pages',
-                '@i18n': '/src/i18n',
-                '@utils': '/src/utils',
-                '@assets': '/src/assets',
-                '@icons': '/src/icons',
-                '@config': '/src/config'
-            }
-        }
-    },
+	viewTransitions: true,
+	vite: {
+		plugins: [tailwindcss()],
+		resolve: {
+			alias: {
+				'@components': '/src/components',
+				'@layouts': '/src/layouts',
+				'@pages': '/src/pages',
+				'@i18n': '/src/i18n',
+				'@utils': '/src/utils',
+				'@assets': '/src/assets',
+				'@icons': '/src/icons',
+				'@config': '/src/config',
+			},
+		},
+	},
 
-    i18n: {
-        locales: ['ru', 'en'],
-        defaultLocale: 'ru',
-        routing: {
-            prefixDefaultLocale: true
-        }
-    },
+	i18n: {
+		locales: ['ru', 'en'],
+		defaultLocale: 'ru',
+		routing: {
+			prefixDefaultLocale: true,
+		},
+	},
 
-    integrations: [icon(), vue({ devtools: true }), react()],
-    output: 'server',
-    adapter: node({
-        mode: 'standalone'
-    })
+	integrations: [icon(), vue({ devtools: true }), react()],
+	output: 'server',
+	adapter: node({
+		mode: 'standalone',
+	}),
+
+	server: {
+		host: true,
+	},
 })
